0cb9df2b1a6e82450499cac0369b608e28d0f198
Nuno Miguel Nobre <nuno.machado-nobre@stfc.ac.uk>
2022-02-11 17:56:11 +0000

    Update the Nvidia HPC SDK to version 22.2

--- install/get_nemo.sh
+++ install/get_nemo.sh
@@ -14,8 +14,8 @@ mkdir -p $BUILD_DIR
 ###########
 ########### NVIDIA HPC SDK
 ###########
-NVHPC_VERSION=22.1
-CUDA_VERSION=11.5
+NVHPC_VERSION=22.2
+CUDA_VERSION=11.6
 TARGET_ARCH=Linux_x86_64
 
 NVHPC_VSTR=$(sed 's/\.//' <<< $NVHPC_VERSION)
